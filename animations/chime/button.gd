43933c1533b49b7ce2ab4c5b4a43cc45fc993c48
alizahidi1000 <shahabzahidi@icloud.com>
2026-02-13 00:08:02 -0500

    Updates to the Chime animation

--- animations/chime/button.gd
+++
@@ -1,7 +0,0 @@
-extends Node3D
-
-
-func _on_button_pressed() -> void:
-	$SubViewportContainer/SubViewport/AnimationPlayer.stop()
-	$SubViewportContainer/SubViewport/AnimationPlayer.play("new_animation")
-	$SubViewportContainer/SubViewport/AnimationPlayer.seek(0, true)

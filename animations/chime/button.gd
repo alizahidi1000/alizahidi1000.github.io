1588f76f2ca771cece1d4b3f9dc10186f33db5f9
alizahidi1000 <shahabzahidi@icloud.com>
2026-02-14 23:08:24 -0500

    Made some fixes and changes

--- animations/chime/button.gd
+++ animations/chime/button.gd
@@ -6,20 +6,22 @@ extends Node3D
 
 func _ready():
 	$ResourcePreloader/SubViewportContainer/SubViewport/ColorRect3.visible = true
-	$ResourcePreloader/SubViewportContainer2/SubViewport2/AudioStreamPlayer2D.playing = false
+	audiostreamplayer.playing = false
 	animationplayer.play ("new_animation")
 	animationplayer.speed_scale = 10
 	animationplayer2.speed_scale = 10
-
-func _on_animation_player_animation_finished(_anim_name = "new_animation"):
-	$ResourcePreloader/SubViewportContainer/SubViewport/ColorRect3.visible = false
+	await get_tree().create_timer(5).timeout
+	animationplayer.stop()
 	animationplayer.speed_scale = 1.0
 	animationplayer2.speed_scale = 1.0
 	animationplayer.play("title")
+	$ResourcePreloader/SubViewportContainer/SubViewport/ColorRect3.visible = false
+	audiostreamplayer.playing = false
 	await get_tree().create_timer(1).timeout
 	animationplayer.stop()
 	animationplayer.seek (1, true)
 
 func _on_button_pressed() -> void:
 	animationplayer.stop()
+	animationplayer.seek (0, true)
 	animationplayer.play("new_animation")
